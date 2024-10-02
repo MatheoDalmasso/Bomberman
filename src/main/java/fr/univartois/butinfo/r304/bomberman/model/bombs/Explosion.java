b04fbac7c4fe4bb0d7e5efced98b5d107c458368
matheo.dalmasso <matheo.dalmasso@info0627.ipa.iutlens.local>
2024-10-02 09:48:23 +0200

    Fixes #6 - Création du constructeur de la classe Explosion

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/bombs/Explosion.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/bombs/Explosion.java
@@ -1,9 +1,23 @@
 package fr.univartois.butinfo.r304.bomberman.model.bombs;
 
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
 import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
 
 public class Explosion extends AbstractMovable {
+
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Explosion(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
 
     @Override
     public void collidedWith(IMovable other) {
