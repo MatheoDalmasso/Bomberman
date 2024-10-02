cc69d31001af278305fe26a7e09314ac8ea429d4
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-02 09:48:48 +0200

    Fixes #7 - Mise en place du constructeur

--- src/main/java/fr/univartois/butinfo/r304/bomberman/joueur/Joueur.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/joueur/Joueur.java
@@ -1,10 +1,16 @@
 package fr.univartois.butinfo.r304.bomberman.joueur;
 
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
 import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
 
 public class Joueur extends AbstractMovable {
 
+
+    public Joueur(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
 
     @Override
     public void collidedWith(IMovable other) {
