2fe21c717bcafbfcc5df4f3c9718b3b79cfa8503
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-02 09:42:39 +0200

    Fixes #3 - Creation de la classe Joueur et du package joueur et heritage de la classe AbstractMovable

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/joueur/Joueur.java
@@ -0,0 +1,23 @@
+package fr.univartois.butinfo.r304.bomberman.joueur;
+
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
+
+public class Joueur extends AbstractMovable {
+
+
+    @Override
+    public void collidedWith(IMovable other) {
+
+    }
+
+    @Override
+    public void explode() {
+
+    }
+
+    @Override
+    public void hitEnemy() {
+
+    }
+}
