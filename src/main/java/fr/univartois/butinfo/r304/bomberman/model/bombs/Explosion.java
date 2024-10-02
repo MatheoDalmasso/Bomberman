39e25b8f98c529a4db12ddb8d5e6a9ecd97a6eac
matheo.dalmasso <matheo.dalmasso@info0627.ipa.iutlens.local>
2024-10-02 09:44:39 +0200

    Fixes #2 - Creation de la classe Explosion et heritage de la classe AbstractMovable

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/bombs/Explosion.java
@@ -0,0 +1,22 @@
+package fr.univartois.butinfo.r304.bomberman.model.bombs;
+
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
+
+public class Explosion extends AbstractMovable {
+
+    @Override
+    public void collidedWith(IMovable other) {
+
+    }
+
+    @Override
+    public void explode() {
+
+    }
+
+    @Override
+    public void hitEnemy() {
+
+    }
+}
