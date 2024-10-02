d955f6b0502f54acf7bee583d8bc45e4df6a1b4d
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-02 10:07:18 +0200

    Fixes #5 - Creation des methodes d'acces , initialisation des attributs dans le constructeur et mise en place des methodes incrementScore , decrementScore

--- src/main/java/fr/univartois/butinfo/r304/bomberman/joueur/Joueur.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/joueur/Joueur.java
@@ -1,16 +1,26 @@
 package fr.univartois.butinfo.r304.bomberman.joueur;
 
+import com.sun.javafx.image.IntPixelGetter;
 import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
 import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
 import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 public class Joueur extends AbstractMovable {
+
+    private IntegerProperty score;
+
+    private IntegerProperty pointsDeVie;
 
 
     public Joueur(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
         super(game, xPosition, yPosition, sprite);
+        this.score = new SimpleIntegerProperty(0);
+        this.pointsDeVie = new SimpleIntegerProperty(3);
     }
+
 
     @Override
     public void collidedWith(IMovable other) {
@@ -25,5 +35,37 @@ public class Joueur extends AbstractMovable {
     @Override
     public void hitEnemy() {
 
+    }
+
+    public IntegerProperty scoreProperty() {
+        return score;
+    }
+
+    public IntegerProperty pointsDeVieProperty() {
+        return pointsDeVie;
+    }
+
+    public int getScore() {
+        return score.get();
+    }
+
+    public void setScore(int score) {
+        this.score.set(score);
+    }
+
+    public int getPointsDeVie() {
+        return pointsDeVie.get();
+    }
+
+    public void setPointsDeVie(int pointsDeVie) {
+        this.pointsDeVie.set(pointsDeVie);
+    }
+
+    public void incrementScore(int score){
+        this.score.set(this.score.get() + score);
+    }
+
+    public void decrementPointsDeVie(int pointsDeVie){
+        this.pointsDeVie.set(this.pointsDeVie.get() - pointsDeVie);
     }
 }
