d8003ae5af831d3b78b197b7b8b2e5c6f50a7cdd
matheo.dalmasso <matheo.dalmasso@info0706.ipa.iutlens.local>
2024-10-08 15:07:06 +0200

    Fixes #22/23 - Création de l'attribut, d'une fonction permettant de gérer le dépot de la bombe + méthode move

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/bombs/Bombe.java
@@ -0,0 +1,54 @@
+package fr.univartois.butinfo.r304.bomberman.model.bombs;
+
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+import javafx.scene.image.Image;
+
+public class Bombe extends AbstractMovable {
+
+    private long timerApparition;
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Bombe(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
+
+    public long dropBomb() {
+        timerApparition = System.currentTimeMillis();
+        return timerApparition;
+    }
+
+    @Override
+    public boolean move(long delta) {
+        if (System.currentTimeMillis() - dropBomb() > 4000) {
+            game.removeMovable(this);
+            Explosion explosion = new Explosion(game, getX(), getY(), new Sprite(new Image("explosion.png")));
+            game.addMovable(explosion);
+        }
+        return true;
+    }
+
+    @Override
+    public void collidedWith(IMovable other) {
+
+    }
+
+    @Override
+    public void explode() {
+
+    }
+
+    @Override
+    public void hitEnemy() {
+
+    }
+
+}
