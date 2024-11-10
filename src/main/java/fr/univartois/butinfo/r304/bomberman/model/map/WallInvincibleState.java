6c0fd9320f0c90dc1899bec68d353964276b5e1c
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-11-10 21:01:41 +0100

    #75 - création des murs invincibles

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/map/WallInvincibleState.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/map/WallInvincibleState.java
@@ -1,4 +1,24 @@
 package fr.univartois.butinfo.r304.bomberman.model.map;
 
-public class MurInvincibleState {
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+
+public class WallInvincibleState implements IWallState {
+
+    private final Sprite sprite;
+
+    public WallInvincibleState(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    @Override
+    public void degrade(Wall wall) {
+        // Do nothing, mur incassable
+    }
+
+    @Override
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+
 }
