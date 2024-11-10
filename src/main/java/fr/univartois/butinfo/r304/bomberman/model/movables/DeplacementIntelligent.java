49e825829ea574fca60f1a53206c66ff53ce7f4b
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-11-10 15:22:43 +0100

    #72 ajout de javadoc

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/DeplacementIntelligent.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/DeplacementIntelligent.java
@@ -1,16 +1,47 @@
+/**
+ * Classe DeplacementIntelligent : permet de déplacer un personnage ennemi de manière intelligente.
+ */
 package fr.univartois.butinfo.r304.bomberman.model.movables;
 
+/**
+ * Classe DeplacementIntelligent : permet de déplacer un personnage ennemi de manière intelligente.
+ */
 public class DeplacementIntelligent implements DeplacementStrategy {
+    /**
+     * Le joueur pour lequel l'ennemi doit se déplacer.
+     */
     private Joueur player; // Permet de recup la pos du player
 
+    /**
+     * Temps de mouvement d'un ennemi.
+     */
     private static final int TEMPS_MOUVEMENT = 2000;
+
+    /**
+     * La distance en pixels du mouvement.
+     */
     private static final double DISTANCE_PIXELS_DU_MOUVEMENT = 36;
+
+    /**
+     * La vitesse de déplacement.
+     */
     private static final double SPEED = DISTANCE_PIXELS_DU_MOUVEMENT / TEMPS_MOUVEMENT * 1000;
 
+    /**
+     * Crée une nouvelle instance de DeplacementIntelligent.
+     *
+     * @param player Le joueur pour lequel l'ennemi doit se déplacer.
+     */
     public DeplacementIntelligent(Joueur player) {
         this.player = player;
     }
 
+    /**
+     * Permet de déplacer l'ennemi de manière intelligente.
+     *
+     * @param ennemi L'ennemi à déplacer.
+     * @param delta  Le temps écoulé depuis le dernier déplacement de cet objet (en
+     */
     @Override
     public void deplacer(PersonnageEnnemi ennemi, long delta) {
         double xPlayer = player.getX();
