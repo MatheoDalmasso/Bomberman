eb0a6a2c23a34182d7d3e172eb86cc186d399b34
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-11-25 11:00:41 +0100

    #78 - rajout du code pour le bonus de la bombe

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/bonus/RecupBombe.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/bonus/RecupBombe.java
@@ -1,4 +1,51 @@
 package fr.univartois.butinfo.r304.bomberman.model.bonus;
 
-public class RecupBombe {
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.model.bombs.Bombe;
+import fr.univartois.butinfo.r304.bomberman.model.map.IWallState;
+import fr.univartois.butinfo.r304.bomberman.model.map.Wall;
+import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
+import fr.univartois.butinfo.r304.bomberman.model.movables.Joueur;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+
+public class RecupBombe extends AbstractMovable implements IWallState {
+
+    private Bombe bombe;
+
+
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected RecupBombe(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+        bombe = new Bombe(game, xPosition, yPosition, sprite, 4000);
+    }
+
+    @Override
+    public void collidedWith(IMovable other) {
+        if (other instanceof Joueur joueur) {
+            joueur.addBombe(bombe);
+        }
+    }
+
+    @Override
+    public void explode() {
+        //
+    }
+
+    @Override
+    public void hitEnemy() {
+        //
+    }
+
+    @Override
+    public void degrade(Wall wall) {
+        //
+    }
 }
