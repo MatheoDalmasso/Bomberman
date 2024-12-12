593957af506786e9d832fcec00d911667486db1d
mathias_morel <mathias_morel@ens.univ-artois.fr>
2024-12-12 09:17:35 +0100

    Fixes #128 - Initialisation du module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,12 @@ module fr.univartois.butinfo.r304.bomberman {
     exports fr.univartois.butinfo.r304.bomberman.model.map.mapgenerator;
     exports fr.univartois.butinfo.r304.bomberman.model.map.mapgenerator.factory;
     exports fr.univartois.butinfo.r304.bomberman.view;
+    exports fr.univartois.butinfo.r304.bomberman.controller to javafx.fxml;
 
     requires transitive javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.logging;
     opens fr.univartois.butinfo.r304.bomberman to javafx.fxml;
+    opens fr.univartois.butinfo.r304.bomberman.controller to javafx.fxml;
 }
